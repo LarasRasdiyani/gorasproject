a0a8be5bbf1f1241938b51c80a0ad955310a16bc
LarasRasdiyani <54461037+LarasRasdiyani@users.noreply.github.com>
2019-12-27 20:56:05 +0700

    Update GORAS Ludo Games.c

--- Goras Ludo Games Project/GORAS Ludo Games.c
+++ Goras Ludo Games Project/GORAS Ludo Games.c
@@ -133,7 +133,7 @@ void header() { //show the big text of GORAS
 	char temp[200];
 	
 	//openfile
-	FPread=fopen("header.dat","r");
+	FPread=fopen("file\\header.txt","r");
 	while(fgets(temp,sizeof(temp),FPread)) {
 		printf("%s",temp);
 	}fclose(FPread);
@@ -144,9 +144,10 @@ void intro() { //showing the welcome words
 	//Declare
 	int i,j;
 	int len;
-	char wlc[50] = {"WELCOME TO GORAS LUDO GAMES!!!"};
-	char line[50] = {"=============================="};
+	char wlc[50] = {"SELAMAT DATANG DI GORAS LUDO GAMES!!!"};
+	char line[50] = {"======================================"};
 	
+	header();
 	//loop
 	gotoxy(68,24);
 	len = strlen(line);
@@ -177,7 +178,7 @@ void mapBesar() { //show the map
 	system("cls");
 	FILE *FPRead;
 	char temporary[200];
-	FPRead=fopen("MAP.txt","r");
+	FPRead=fopen("file\\MAP.txt","r");
 	while(fgets(temporary,sizeof(temporary),FPRead)) {
 		printf("%s",temporary);
 	} fclose(FPRead);	
@@ -203,6 +204,7 @@ void isGameOver() {
 
 /*=============== MAIN MENU ===============*/
 void mainMenu() { //main menu
+	header();
 	//Declare
 	int input;
 	gotoxy(100,19);printf(" _______________________");
@@ -228,50 +230,36 @@ void mainMenu() { //main menu
 	gotoxy(73,20);printf("====================");
 	gotoxy(73,21);printf("Main Menu");
 	gotoxy(73,22);printf("====================");
-	gotoxy(73,24);printf("(1) Start Game");
-	gotoxy(73,25);printf("(2) Instruction");
-	gotoxy(73,26);printf("(3) Credits");
-	gotoxy(73,27);printf("(4) Exit Game");
-	gotoxy(73,28);printf("(5) Lihat Map");
-	gotoxy(65,34);printf("Insert number where u want to go = ");scanf("%d", &input);
-	if (input==1 || input==2 || input==3 || input==4 || input==5) {
-		if (input==1) {
-			system("CLS");
-			pickPlayer();
-		}
-		if (input==2) {
-			system("CLS");
-			instruction();
-		}
-		if (input==3) {
-			system("CLS");
-			credits();
-		}
-		if (input==4) {
-			exit(1);
-		}
-		if (input==5) {
-			mapBesar();
-		}
+	gotoxy(73,24);printf("[1] Mulai Bermain");
+	gotoxy(73,25);printf("[2] Cara Bermain");
+	gotoxy(73,26);printf("[3] Credits");
+	gotoxy(73,27);printf("[4] Keluar Permainan");
+	gotoxy(65,34);printf("Masukkan angka yang akan anda tuju = ");scanf("%d", &input);
+	switch(input){
+		case 1 : system("CLS");selectPlayer();break;
+		case 2 : system("CLS");instruction();break;
+		case 3 : system("CLS");credits();break;		 
+		case 4 : exit(1);break;
+		default: gotoxy(65,34);printf("Maaf angka yang anda inputkan tidak sesuai");
+		         gotoxy(65,36);system("PAUSE");mainMenu();break;
 	}
 } //end main menu
 
 /*=============== Pick Player ===============*/
-void pickPlayer() { //how many player
+void selectPlayer() { //how many player
+	system("CLS");
 	header();
-	
 	int input;
-	
-	gotoxy(100,19);printf(" _______________________");
-	gotoxy(100,20);printf("|                       |");
-	gotoxy(100,21);printf("|                       |");
-	gotoxy(100,22);printf("|      O         O      |");
-	gotoxy(100,23);printf("|                       |");
-	gotoxy(100,24);printf("|      O         O      |");
-	gotoxy(100,25);printf("|                       |");
-	gotoxy(100,26);printf("|      O         O      |");
-	gotoxy(100,27);printf("|                       |");
-	gotoxy(100,28);printf("|_______________________|");
+	gotoxy(108,19);printf(" _______________________");
+	gotoxy(108,20);printf("|                       |");
+	gotoxy(108,21);printf("|                       |");
+	gotoxy(108,22);printf("|      O         O      |");
+	gotoxy(108,23);printf("|                       |");
+	gotoxy(108,24);printf("|      O         O      |");
+	gotoxy(108,25);printf("|                       |");
+	gotoxy(108,26);printf("|      O         O      |");
+	gotoxy(108,27);printf("|                       |");
+	gotoxy(108,28);printf("|_______________________|");
 	gotoxy(40,19);printf(" _______________________");
 	gotoxy(40,20);printf("|                       |");
 	gotoxy(40,21);printf("|                       |");
@@ -282,38 +270,27 @@ void pickPlayer() { //how many player
 	gotoxy(40,26);printf("|      O         O      |");
 	gotoxy(40,27);printf("|                       |");
 	gotoxy(40,28);printf("|_______________________|");
-	gotoxy(73,20);printf("====================");
-	gotoxy(73,21);printf("Jumlah Pemain");
-	gotoxy(73,22);printf("====================");
-	gotoxy(73,24);printf("(1) 2 Pemain");
-	gotoxy(73,25);printf("(2) 3 Pemain");
-	gotoxy(73,26);printf("(3) 4 Pemain");
-	gotoxy(73,27);printf("(4) Kembali ke menu");
-	gotoxy(73,28);printf("(5) Keluar dari permainan");
-	gotoxy(65,33);printf("Masukan angka yang ingin dituju = ");scanf("%d", &input);
-	if (input==1 || input==2 || input==3 || input==4 || input==5) {
-		if (input==1) {
-			system("CLS");
-			gamePlay();
-		}
-		if (input==2) {
-			system("CLS");
-			instruction();
-		}
-		if (input==3) {
-			system("CLS");
-			credits();
-		}
-		if (input==4) {
-			system("CLS");
-			header();
-			mainMenu();
-		}
-		if (input==5) {
-			system("exit");
-		}
-	}
-
+	gotoxy(73,20);printf("============================");
+	gotoxy(73,22);printf("     [1]  2 Pemain ");
+	gotoxy(73,23);printf("     [2]  3 Pemain ");
+	gotoxy(73,24);printf("     [3]  4 Pemain ");
+	gotoxy(73,25);printf("     [4]  Kembali ke Menu");
+	gotoxy(73,27);printf("============================");
+	gotoxy(70,33);printf("Masukkan angka yang akan anda tuju = ");scanf("%d", &input);
+	switch(input){
+		case 1 : system("CLS");newGames();
+				 break;
+		case 2 : system("CLS");newGames();
+				 break;
+		case 3 : system("CLS");newGames();
+				 break;
+		case 4 : system("CLS");mainMenu();
+				 break;
+		default: gotoxy(70,33);printf("Maaf angka yang anda inputkan tidak sesuai");
+				 gotoxy(70,35);system("PAUSE");
+				 selectPlayer();
+				 break;
+	}	
 }
 
 /*=============== in Game ===============*/
@@ -329,7 +306,7 @@ void instruction() { //instruction page
 	FILE *FPRead;
 	char Temporary[200];
 	short i=16;
-	FPRead=fopen("instruction.txt","r");
+	FPRead=fopen("file\\instruction.txt","r");
 	while(fgets(Temporary,sizeof(Temporary),FPRead)){
 		gotoxy(1,i);
 		printf("%s",Temporary);
@@ -344,9 +321,25 @@ void instruction() { //instruction page
 } //end instruction
 
 /*=============== Credits ===============*/
-void credits() { //credits main
-	printf("credits");
-}
+void credits(){//credits main
+	system("CLS");
+	header(); 
+	FILE *FPRead;
+	char Temporary[100];
+	short i=20;
+	FPRead=fopen("file\\credits.txt","r");
+	while(fgets(Temporary,sizeof(Temporary),FPRead)){
+		gotoxy(50,i);
+		printf("%s",Temporary);
+		i++;
+	}
+	fclose(FPRead);
+	
+	gotoxy(80,38);
+	system("PAUSE");
+	system("CLS");
+	mainMenu();
+} //end credits
 
 /*=============== Main Control Page ===============*/
 int main() {
